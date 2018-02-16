0cba57658b7e3674a73baf8aa2bbefd7cc1d7dc4
nages004 <nages004@umn.edu>
2018-02-15 18:17:18 -0600

    k

--- hit_stream1.py
+++ hit_stream1.py
@@ -4,7 +4,7 @@ with serial.Serial('/dev/tty.usbmodem1421', 9600, timeout=1) as ser:
     count = 0
     aggregate = 0
     while 1:
-        # aggregate += float(ser.readline().decode("ascii", "ignore").strip())
+        aggregate += float(ser.readline().decode("ascii", "ignore").strip())
         if count == 5:
             requests.post('http://127.0.0.1:5000/api/send_event/Bob',
                           data=str(aggregate / 5))
